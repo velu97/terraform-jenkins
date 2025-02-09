e31250e5efaaeb00b2186c39b825fcc2508b521f
velu97 <136069658+velu97@users.noreply.github.com>
2025-02-09 22:44:17 +0530

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456"
+    bucket = "dev-proj-1-jenkins-remote-state-bucket-7890"
     key    = "devops-project-1/jenkins/terraform.tfstate"
     region = "eu-west-1"
   }
